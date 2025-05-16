80862cb13e2bea63e38fad7b53662f5e294821a6
Cristóbal Veas <cristobal.veas@proton.me>
2025-05-16 12:24:57 -0400

    Corregir diagrama estrella y otras cosas

--- queries/tables.sql
+++ queries/tables.sql
@@ -1,11 +1,6 @@
-CREATE TABLE "vendedores" (
-  "id_vendedor" integer PRIMARY KEY,
-  "nombre" varchar,
-  "correo" varchar
-);
-
 CREATE TABLE "hechos_ventas" (
-  "id_venta" integer PRIMARY KEY,
+  "id" serial PRIMARY KEY,
+  "id_venta" integer,
   "monto_total" integer,
   "id_cliente" integer,
   "fecha" timestamp,
@@ -15,36 +10,42 @@ CREATE TABLE "hechos_ventas" (
   "cantidad" integer
 );
 
+CREATE TABLE "vendedores" (
+  "id_vendedor" serial PRIMARY KEY,
+  "nombre" varchar,
+  "correo" varchar
+);
+
+CREATE TABLE "productos" (
+  "id_producto" serial PRIMARY KEY,
+  "precio" integer,
+  "nombre" varchar
+);
+
 CREATE TABLE "clientes" (
-  "id_cliente" integer PRIMARY KEY,
+  "id_cliente" serial PRIMARY KEY,
   "nombre" varchar,
   "genero" varchar
 );
 
-CREATE TABLE "productos" (
-  "id_producto" integer PRIMARY KEY,
-  "precio" integer,
-  "nonbre" varchar
-);
-
 CREATE TABLE "hechos_compras" (
-  "id_gasto" integer PRIMARY KEY,
+  "id_gasto" serial PRIMARY KEY,
   "id_insumo" integer,
   "cantidad" integer,
   "fecha" timestamp
 );
 
 CREATE TABLE "insumos" (
-  "id_insumo" integer PRIMARY KEY,
+  "id_insumo" serial PRIMARY KEY,
   "nombre" varchar,
   "precio" integer,
   "medida" varchar
 );
 
-ALTER TABLE "vendedores" ADD FOREIGN KEY ("id_vendedor") REFERENCES "hechos_ventas" ("id_vendedor");
-
 ALTER TABLE "hechos_ventas" ADD FOREIGN KEY ("id_cliente") REFERENCES "clientes" ("id_cliente");
+
+ALTER TABLE "hechos_ventas" ADD FOREIGN KEY ("id_vendedor") REFERENCES "vendedores" ("id_vendedor");
 
 ALTER TABLE "hechos_ventas" ADD FOREIGN KEY ("id_producto") REFERENCES "productos" ("id_producto");
 
-ALTER TABLE "insumos" ADD FOREIGN KEY ("id_insumo") REFERENCES "hechos_compras" ("id_insumo");
+ALTER TABLE "hechos_compras" ADD FOREIGN KEY ("id_insumo") REFERENCES "insumos" ("id_insumo");
